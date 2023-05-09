9453b1991a851ef03da28e33945db21f51bd5101
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-09 11:52:04 +0530

    add two numbers

---
+++ add two numbers/script.js
@@ -0,0 +1,10 @@
+let a=5
+let b=6
+ 
+let c=a+b
+console.log(`the sume of ${a} and ${b} is =>${c}`)
+
+let x=prompt("enter the first number")
+let y=prompt("enter the second number")
+let z=parseInt(x)+parseInt(y)
+console.log(`the sum of ${x} and ${y} is =>${z}`)
