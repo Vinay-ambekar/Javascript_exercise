d2228febf9bfc9a69696dcb622541cf6d4bb27c2
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-05-12 11:04:04 +0530

    swap two number

---
+++ 04swapping two numbers/script.js
@@ -0,0 +1,19 @@
+/* var a=prompt('enter the number')
+var b=prompt('enter the number')
+console.log(`the value of a is:${a} the value of b is ${b}`)
+var temp;
+temp=a;
+a=b;
+
+b=temp
+console.log(`the value of a is:${a} the value of b is ${b}`) */
+ //second mehtod 
+ let x=parseInt(prompt("enter the biger number ") )
+
+ let y= parseInt(prompt("enter the smaller number")) 
+ console.log(`x is: ${x} y is: ${y}`)
+ x=x+y
+ y=x-y
+ x=x-y
+ console.log(`x is: ${x} \n y is: ${y}`)
+ 
